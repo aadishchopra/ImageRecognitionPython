2d5b19451addb099413105397d9b71bc320cdfe8
Aadish Chopra <aadishchopra@rediffmail.com>
2019-04-14 18:50:42 -0500

    latest extension of vs

--- IR/bootstrap.py
+++ IR/bootstrap.py
@@ -4,7 +4,10 @@ import pip
 
 # Check if pip is installed by running this on command line
 # pip
-# You might have to set environment variables for python. 
+# You might have to set environment variables for python. # first import os
+
+#os.system('Set')
+
 
 #See the difference below
 
@@ -17,6 +20,9 @@ s=pd.Series([1, 3, 5, 6, 8])
 import os
 
 # It sends commands to command prompt
+
+#We are using system calls because it helps in automation and deployment 
+
 os.system('pip install numpy')
 os.system('pip list')
 
@@ -31,7 +37,21 @@ x=np.linspace(0,10,5)
 
 # above code was an indication that we successfully installed 
 
+# to install scikit-learn
+os.system('pip install -U scikit-learn')
+# Let's begin our project on Image Recogntion 
+os.system('python -m pip install Pillow')
+# this seems to be working. 
+from PIL import Image
 
+i=Image.open('images/dot.png')
+iar=np.asarray(i)
+print(iar)
 
+# Run below code to clear your terminal 
+os.system("cls")
+
+# to see what all modules have been installed 
+os.system('pip freeze')
 
 
---
+++ IR/helper.py
@@ -0,0 +1,33 @@
+# Run below code to clear your terminal 
+import os 
+os.system("cls")
+
+# Write a function to load all the required libraries at startup
+
+def load_libraries_on_startup():
+
+    import os 
+    base_packages=['pip']
+    packages_list=['numpy','scipy','scikit-learn','Pillow']
+    base_len=len(base_packages)
+    pack_len=len(packages_list)
+
+    for package in range(base_len):
+        print("Importing "+base_packages[package])
+        exec('import ' +base_packages[package])
+        
+    for package in range(pack_len):
+        print("Installing "+packages_list[package])
+        os.system('pip install '+packages_list[package])
+        print("Importing "+packages_list[package])
+        if(packages_list[package]=="scikit-learn"):
+            exec('import sklearn')
+        elif(packages_list[package]=="Pillow"):
+            exec('from PIL import Image')
+        else:            
+            exec('import ' +packages_list[package]) 
+        
+
+
+load_libraries_on_startup()
+
